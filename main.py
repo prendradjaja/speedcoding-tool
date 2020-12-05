a17c80982399ed4d443b8a07726d97eaa7d56b39
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-05 13:54:25 -0800

    refac b4 watch

--- main.py
+++ main.py
@@ -3,24 +3,30 @@ from stdoutio import stdoutIO
 from read_messages import is_xpf, parse_xpf
 import sys
 
-path = sys.argv[1]
+def main():
+    path = sys.argv[1]
+    run_and_display(path)
 
-code, line_numbers = rewrite(path)
-with stdoutIO() as s:
-    exec(code)
+def run_and_display(path):
+    code, line_numbers = rewrite(path)
+    with stdoutIO() as s:
+        exec(code)
 
-values = {}
-for line in s.getvalue().splitlines():
-    if is_xpf(line):
-        message = parse_xpf(line)
-        lineno = line_numbers[message.call_id]
-        values[lineno] = message.value
+    values = {}
+    for line in s.getvalue().splitlines():
+        if is_xpf(line):
+            message = parse_xpf(line)
+            lineno = line_numbers[message.call_id]
+            values[lineno] = message.value
 
-WIDTH = 40
+    WIDTH = 40
 
-for i, line in enumerate(open(path), 1):
-    line = line.rstrip()
-    print(line.ljust(WIDTH)[:WIDTH], end='')
-    if i in values:
-        print(values[i], end='')
-    print()
+    for i, line in enumerate(open(path), 1):
+        line = line.rstrip()
+        print(line.ljust(WIDTH)[:WIDTH], end='')
+        if i in values:
+            print(values[i], end='')
+        print()
+
+if __name__ == '__main__':
+    main()
