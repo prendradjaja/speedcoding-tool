9131c04546434ef0b423e29f22314e38c157a450
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-05 13:57:49 -0800

    watch!

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
+import sys
+import time
+import os
+
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler, FileSystemEventHandler
+
 from rewriter import rewrite
 from stdoutio import stdoutIO
 from read_messages import is_xpf, parse_xpf
-import sys
+
+
+class Handler(FileSystemEventHandler):
+    def __init__(self, path):
+        self.path = path
+
+    # @staticmethod
+    def on_any_event(self, event):
+        if event.event_type == 'modified':
+            run_and_display(self.path)
 
 def main():
     path = sys.argv[1]
-    run_and_display(path)
+    event_handler = Handler(path)
+    observer = Observer()
+    observer.schedule(event_handler, path)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
 
 def run_and_display(path):
     code, line_numbers = rewrite(path)
@@ -21,6 +46,7 @@ def run_and_display(path):
 
     WIDTH = 40
 
+    os.system('clear')
     for i, line in enumerate(open(path), 1):
         line = line.rstrip()
         print(line.ljust(WIDTH)[:WIDTH], end='')
