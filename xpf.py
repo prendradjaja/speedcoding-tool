5b6845cc4352291e1b49d6bbf22b3652a9c84f57
Pandu Rendradjaja <3111845+prendradjaja@users.noreply.github.com>
2020-12-06 11:13:55 -0800

    Display len for anything with len

--- xpf.py
+++ xpf.py
@@ -1,9 +1,9 @@
 def xpf(expr, call_id):
     if call_id not in xpf.seen:
         xpf.seen.add(call_id)
-        if isinstance(expr, list):
+        try:
             string = f'({len(expr)}) {repr(expr)}'
-        else:
+        except TypeError:
             string = repr(expr)
         print(f'XPF({call_id})XPF', string)
     return expr
